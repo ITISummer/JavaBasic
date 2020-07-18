a07509adacbdcab4e740dafe01c0d64fe9278514
summer <lv.summer@qq.com>
2020-07-18 15:56:26 +0800

    update code

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -2,7 +2,7 @@ package day01;
 
 class HelloChina{
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello,World!");
 	}
 
---
+++ src/day11/java/Creature.java
@@ -0,0 +1,9 @@
+package day11.java;
+
+public class Creature {
+	
+	public void breath(){
+		System.out.println("呼吸");
+	}
+	
+}
